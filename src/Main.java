991f90b1fd41335ec0f2a9fce4c1cc2919282817
Yaroslav <yarik1995fghg@gmail.com>
2023-03-07 18:58:51 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+        byte first = 10;
+        byte second = 2;
+        System.out.println( "Multiplication: " + first * second );
+        System.out.println( "Division of numbers: " + first / second );
+        System.out.println( "Addition: " + (first + second) );
+        System.out.println( "Subtraction: " + (first - second) );
+
+        System.out.println("------------------------------------");
+
+        short a = 200;
+        short b = 50;
+        System.out.println( "Multiplication: " + a * b );
+        System.out.println( "Division of numbers: " + a / b );
+        System.out.println( "Addition: " + (a + b) );
+        System.out.println( "Subtraction: " + (a - b) );
+
+        System.out.println("------------------------------------");
+
+        float c = 21.2f;
+        float d = 2.3f;
+        System.out.println( "Multiplication: " + c * d );
+        System.out.println( "Division of numbers: " + c / d );
+        System.out.println( "Addition: " + (c + d) );
+        System.out.println( "Subtraction: " + (c - d) );
+
+        System.out.println("------------------------------------");
+
+        double d1 = 783.23d;
+        double d2 = 83.23d;
+        System.out.println( "Multiplication: " + d1 * d2 );
+        System.out.println( "Division of numbers: " + d1 / d2);
+        System.out.println( "Addition: " + (d1 + d2) );
+        System.out.println( "Subtraction: " + (d1 - d2) );
+
+    }
+}
